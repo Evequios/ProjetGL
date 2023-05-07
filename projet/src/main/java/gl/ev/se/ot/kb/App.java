a12c6911e5fcaa6370a26c87b8f9988d958c0608
Evequios <kilianbnr@outlook.fr>
2023-05-07 14:59:03 +0200

    Commit initial

---
+++ projet/src/main/java/gl/ev/se/ot/kb/App.java
@@ -0,0 +1,13 @@
+package gl.ev.se.ot.kb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
